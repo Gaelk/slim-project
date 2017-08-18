44ad1dce5415681f1777d954b7d565eb8fff8142
gael <iceberg007@live.fr>
2017-08-18 11:05:12 +0200

    *route: /hello/{name} <= obligatoireavec [/{age}] <= {}param facultatif à cause du /slash dans les []

--- public/index.php
+++ public/index.php
@@ -7,12 +7,27 @@ use Slim\Http\Response;
 $app= new Slim\App();
 //definnition des routes
 $app->get("/hello", function(Request $request, Response $reponse){
-    /**
+    /** route /hello : avc param dans URL ou pas
      * recuperation de param ici"name" ajouté dans URL si il n'existe pas
      * la variable $param sera "word"
      */
     $param= $request->getParam("name")??"world";
-    return $reponse->getBody()->write("hello $param");
+    return $reponse->getBody()->write("<h1>hello $param</h1>");
 });
+
+/**
+ *route: /hello/{name} <= obligatoire
+ * [/{age}] <= {}param facultatif à cause du /slash dans les []
+ */
+$app->get("/hello/{name}[/{age:\d{1,2}}]",function(Request $request, Response $reponse, array $args){
+    $html= "<h1>hello ". $args["name"]. "</h1>";
+    if(isset($args["age"])){
+        $html .="vous avez {$args["age"]} ans";
+    }
+    return $reponse->getBody()->write($html);
+});
+
+
+
 //Execution de l'application
 $app->run();
